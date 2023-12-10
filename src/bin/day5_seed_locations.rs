c43495290f71ca7e0ee273716eb97a9d0ddc639f
Daniel Lambert <danjl1100@gmail.com>
2023-12-09 20:15:33 -0600

    [WIP] add day5_seed_locations part 2, but hangs for test input

--- src/bin/day5_seed_locations.rs
+++ src/bin/day5_seed_locations.rs
@@ -6,14 +6,33 @@ fn main() -> anyhow::Result<()> {
     println!("hello farmer");
 
     let input = advent_2023::get_input_string()?;
+
+    let closest_location = get_closest_location(&input)?;
+    println!("closest location: {closest_location}");
+
+    Ok(())
+}
+fn get_closest_location(input: &str) -> anyhow::Result<u64> {
     let mut input_lines = input.lines();
 
     let seeds_line = input_lines.next().expect("seeds line");
     let blank_line = input_lines.next().expect("at least one map");
     assert!(blank_line.is_empty());
 
-    let seeds = parse_seeds(seeds_line).with_context(|| format!("seeds line {seeds_line:?}"))?;
+    let seeds_entries =
+        parse_seeds(seeds_line).with_context(|| format!("seeds line {seeds_line:?}"))?;
     let maps = parse_maps(input_lines)?;
+    println!("Loaded maps. Seed entries {seeds_entries:?}");
+
+    let seeds: Vec<_> = seeds_entries
+        .chunks(2)
+        .flat_map(|entries| {
+            let [start, len] = entries.try_into().expect("chunks of 2");
+            start..(start + len)
+        })
+        .collect();
+
+    println!("There are {} seeds.", seeds.len());
 
     assert!(!seeds.is_empty());
 
@@ -34,11 +53,13 @@ fn main() -> anyhow::Result<()> {
         })
         .collect::<Result<Vec<_>, _>>()?;
 
-    let closest_location = locations.iter().min().expect("at least one seed provided");
+    let closest_location = locations
+        .iter()
+        .copied()
+        .min()
+        .expect("at least one seed provided");
 
-    println!("closest location: {closest_location}");
-
-    Ok(())
+    Ok(closest_location)
 }
 
 fn parse_seeds(line: &str) -> anyhow::Result<Vec<u64>> {
@@ -121,10 +142,7 @@ fn parse_range(line: &str) -> anyhow::Result<Range> {
         dest_start - source_start
     };
 
-    let sources = {
-        dbg!(source_start, len);
-        source_start..(source_start + len)
-    };
+    let sources = source_start..(source_start + len);
 
     Ok(Range { sources, offset })
 }
@@ -155,4 +173,49 @@ impl Map {
 struct Range {
     sources: std::ops::Range<u64>,
     offset: i64,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::get_closest_location;
+
+    #[test]
+    fn sample_input() {
+        let input = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+
+        let closest = get_closest_location(input).unwrap();
+        assert_eq!(closest, 46);
+    }
 }
