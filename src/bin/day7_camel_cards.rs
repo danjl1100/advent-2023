08ecb41b6ba2d3dc0b151e5e1ebc7f27b1d65022
Daniel Lambert <danjl1100@gmail.com>
2023-12-20 15:52:17 -0600

    working toward day7_camel_cards part2 solution, not correct yet

--- src/bin/day7_camel_cards.rs
+++ src/bin/day7_camel_cards.rs
@@ -5,22 +5,40 @@ fn main() -> anyhow::Result<()> {
 
     let input = advent_2023::get_input_string()?;
 
-    let total_winnings = analyze_hands(&input)?;
+    let total_winnings = analyze_hands(&input, Rules::Jokers)?;
     println!("Total winnings for all hands: {total_winnings}");
 
     Ok(())
 }
 
-fn analyze_hands(input: &str) -> anyhow::Result<u32> {
+#[derive(Clone, Copy, Debug)]
+enum Rules {
+    #[allow(unused)]
+    Jacks,
+    Jokers,
+}
+
+fn analyze_hands(input: &str, rules: Rules) -> anyhow::Result<u32> {
     let mut hands = input
         .lines()
-        .map(parse_hand)
+        .map(|line| parse_hand(line, rules))
         .collect::<Result<Vec<_>, _>>()?;
-    dbg!(&hands);
+
+    // // DEBUG
+    // println!("Input Hands:");
+    // for (hand, Bid(bid)) in &hands {
+    //     let Hand { cards, ty } = hand;
+    //     println!("\tHand {cards:?} {ty:?}, bid {bid}");
+    // }
 
     hands.sort_by_key(|&(hand, _bid)| hand);
 
-    dbg!(("sorted", &hands));
+    // DEBUG
+    println!("Sorted Hands:");
+    for (hand, Bid(bid)) in &hands {
+        let Hand { cards, ty } = hand;
+        println!("\tHand {cards:?} {ty:?}, bid {bid}");
+    }
 
     let total_winnings = hands
         .into_iter()
@@ -32,7 +50,7 @@ fn analyze_hands(input: &str) -> anyhow::Result<u32> {
         .sum();
     Ok(total_winnings)
 }
-fn parse_hand(line: &str) -> anyhow::Result<(Hand, Bid)> {
+fn parse_hand(line: &str, rules: Rules) -> anyhow::Result<(Hand, Bid)> {
     let mut line_parts = line.split_whitespace();
     let Some(hand) = line_parts.next() else {
         anyhow::bail!("empty line")
@@ -44,8 +62,25 @@ fn parse_hand(line: &str) -> anyhow::Result<(Hand, Bid)> {
         anyhow::bail!("unexpected part on line {line:?}: {extra:?}")
     }
 
-    let hand = hand.parse().map_err(|s| anyhow::anyhow!("hand {s:?}"))?;
+    let hand: Hand = hand.parse().map_err(|s| anyhow::anyhow!("hand {s:?}"))?;
     let bid = bid.parse().map_err(|s| anyhow::anyhow!("bid {s:?}"))?;
+
+    let debug_original_ty = hand.ty;
+
+    let hand = match rules {
+        Rules::Jacks => hand,
+        Rules::Jokers => hand.reinterpret_as_jokers(),
+    };
+    if hand.ty != debug_original_ty {
+        let cards_str: String = hand.cards.iter().copied().map(|c| format!("{c}")).collect();
+        let ty = hand.ty;
+        let direction = if ty > debug_original_ty {
+            "higher"
+        } else {
+            "lower"
+        };
+        eprintln!("{cards_str:?} changed {direction} from {debug_original_ty:?} to {ty:?}");
+    }
 
     Ok((hand, Bid(bid)))
 }
@@ -57,6 +92,16 @@ struct Bid(u32);
 struct Hand {
     cards: [Card; 5],
     ty: Type,
+}
+impl Hand {
+    fn reinterpret_as_jokers(self) -> Self {
+        let Self { mut cards, ty: _ } = self;
+        for card in cards.iter_mut() {
+            *card = card.reinterpret_as_joker();
+        }
+        let ty = Type::from(&cards);
+        Self { cards, ty }
+    }
 }
 impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
@@ -95,48 +140,59 @@ enum Type {
 }
 impl From<&[Card; 5]> for Type {
     fn from(cards: &[Card; 5]) -> Self {
-        let counts = {
-            let mut counts: BTreeMap<Card, u8> = BTreeMap::new();
-            for card in cards {
-                let count = counts.entry(*card).or_default();
-                *count += 1;
+        let mut counts: BTreeMap<Card, usize> = BTreeMap::new();
+        for card in cards {
+            let count = counts.entry(*card).or_default();
+            *count += 1;
+        }
+
+        let count_wild = counts.get(&Card::Joker).copied().unwrap_or_default();
+
+        'outer: for n in (0..5).rev() {
+            for (&card, count) in counts.iter_mut() {
+                if *count == n && card != Card::Joker {
+                    *count += count_wild;
+                    break 'outer;
+                }
             }
-            counts
-        };
+        }
 
         let count_5 = counts.iter().filter(|(_, &count)| count == 5).count();
         let count_4 = counts.iter().filter(|(_, &count)| count == 4).count();
         let count_3 = counts.iter().filter(|(_, &count)| count == 3).count();
         let count_2 = counts.iter().filter(|(_, &count)| count == 2).count();
-        let count_1 = counts.iter().filter(|(_, &count)| count == 1).count();
+        // let count_1 = counts.iter().filter(|(_, &count)| count == 1).count();
 
-        debug_assert_eq!(
-            5 * count_5 + 4 * count_4 + 3 * count_3 + 2 * count_2 + count_1,
-            5
-        );
+        // no longer accurate with wild cards
+        // debug_assert_eq!(
+        //     5 * count_5 + 4 * count_4 + 3 * count_3 + 2 * count_2 + count_1,
+        //     5
+        // );
 
-        if count_5 == 1 {
+        if count_5 >= 1 {
             Self::FiveOfAKind
-        } else if count_4 == 1 {
+        } else if count_4 >= 1 {
             Self::FourOfAKind
-        } else if count_3 == 1 && count_2 == 1 {
+        } else if count_3 >= 1 && count_2 >= 1 {
             Self::FullHouse
-        } else if count_3 == 1 {
+        } else if count_3 >= 1 {
             Self::ThreeOfAKind
-        } else if count_2 == 2 {
+        } else if count_2 >= 2 {
             Self::TwoPair
-        } else if count_2 == 1 {
+        } else if count_2 >= 1 {
             Self::OnePair
         } else {
-            debug_assert_eq!(count_1, 5);
+            // no longer accurate with wild cards
+            // debug_assert_eq!(count_1 - count_wild, 5);
             Self::HighCard
         }
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 // PartialOrd derive: lowest to highest
 enum Card {
+    Joker,
     N2,
     N3,
     N4,
@@ -146,29 +202,37 @@ enum Card {
     N8,
     N9,
     T,
-    J,
+    Jack,
     Q,
     K,
     A,
+}
+impl Card {
+    fn reinterpret_as_joker(self) -> Self {
+        match self {
+            Self::Jack => Self::Joker,
+            other => other,
+        }
+    }
 }
 
 impl TryFrom<char> for Card {
     type Error = char;
     fn try_from(value: char) -> Result<Self, Self::Error> {
         let value = match value {
-            'A' => Self::A,
-            'K' => Self::K,
-            'Q' => Self::Q,
-            'J' => Self::J,
-            'T' => Self::T,
-            '9' => Self::N9,
-            '8' => Self::N8,
-            '7' => Self::N7,
-            '6' => Self::N6,
-            '5' => Self::N5,
-            '4' => Self::N4,
-            '3' => Self::N3,
             '2' => Self::N2,
+            '3' => Self::N3,
+            '4' => Self::N4,
+            '5' => Self::N5,
+            '6' => Self::N6,
+            '7' => Self::N7,
+            '8' => Self::N8,
+            '9' => Self::N9,
+            'T' => Self::T,
+            'J' => Self::Jack, // NOTE: changed to Joker by `reinterpret_as_joker`
+            'Q' => Self::Q,
+            'K' => Self::K,
+            'A' => Self::A,
             _ => {
                 return Err(value);
             }
@@ -176,36 +240,68 @@ impl TryFrom<char> for Card {
         Ok(value)
     }
 }
+impl std::fmt::Display for Card {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let c = match self {
+            Card::Joker => '?',
+            Card::N2 => '2',
+            Card::N3 => '3',
+            Card::N4 => '4',
+            Card::N5 => '5',
+            Card::N6 => '6',
+            Card::N7 => '7',
+            Card::N8 => '8',
+            Card::N9 => '9',
+            Card::T => 'T',
+            Card::Jack => 'J',
+            Card::Q => 'Q',
+            Card::K => 'K',
+            Card::A => 'A',
+        };
+        write!(f, "{c}")
+    }
+}
+impl std::fmt::Debug for Card {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(self, f)
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use std::str::FromStr;
-
-    use crate::{analyze_hands, Card, Hand, Type};
+    use crate::{analyze_hands, parse_hand, Card, Rules, Type};
 
     macro_rules! test_types {
         (
+            $rules:expr;
             $(
                 $str:expr => $expected_ty:expr;
             )+
         ) => {
+            let rules: Rules = $rules;
             $({
-                let hand_str = $str;
-                let hand = Hand::from_str(hand_str).unwrap();
+                let hand_str: &'static str = $str;
+                let (hand, _) = parse_hand(&format!("{hand_str} 0"), rules).expect(hand_str);
                 assert_eq!(hand.ty, $expected_ty, "hand {hand_str} {hand:?}");
             })+
         };
     }
 
-    #[test]
-    fn sample_input() {
-        let input = "32T3K 765
+    const SAMPLE_INPUT: &str = "32T3K 765
 T55J5 684
 KK677 28
 KTJJT 220
 QQQJA 483";
-        let total_winnings = analyze_hands(input).unwrap();
+
+    #[test]
+    fn sample_input() {
+        let total_winnings = analyze_hands(SAMPLE_INPUT, Rules::Jacks).unwrap();
         assert_eq!(total_winnings, 6440);
+    }
+    #[test]
+    fn sample_input_jokers() {
+        let total_winnings = analyze_hands(SAMPLE_INPUT, Rules::Jokers).unwrap();
+        assert_eq!(total_winnings, 5905);
     }
 
     #[test]
@@ -216,11 +312,17 @@ QQQJA 483";
         assert!(a > nine);
         assert!(nine > two);
         assert!(a > two);
+
+        let joker = Card::try_from('J').unwrap().reinterpret_as_joker();
+        assert!(a > joker);
+        assert!(nine > joker);
+        assert!(two > joker);
     }
 
     #[test]
     fn classify_types() {
         test_types! {
+            Rules::Jacks;
             "KKQQ2" => Type::TwoPair;
             "KKQQK" => Type::FullHouse;
             "2222K" => Type::FourOfAKind;
@@ -229,6 +331,20 @@ QQQJA 483";
             "44444" => Type::FiveOfAKind;
             "3459T" => Type::HighCard;
             "666QK" => Type::ThreeOfAKind;
+        }
+    }
+
+    #[test]
+    fn classify_joker_to_highest() {
+        test_types! {
+            Rules::Jokers;
+            // HighCard not possible, will auto-promote to OnePair
+            "2345J" => Type::OnePair;
+            // TwoPair - not possible, will auto-promote to ThreeOfAKind
+            "2234J" => Type::ThreeOfAKind;
+            // FullHouse - not possible, will auto-promote to FourOfAKind
+            "2223J" => Type::FourOfAKind;
+            "2222J" => Type::FiveOfAKind;
         }
     }
 }
