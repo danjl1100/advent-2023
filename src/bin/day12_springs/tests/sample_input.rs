5131262ee59ecbfc5952081f0187982644c8da70
Daniel Lambert <danjl1100@gmail.com>
2023-12-31 11:42:22 -0600

    [WIP] day12 validate test input unfolded, identify caching need

--- src/bin/day12_springs/tests/sample_input.rs
+++ src/bin/day12_springs/tests/sample_input.rs
@@ -1,15 +1,5 @@
 use crate::{day12_springs::record::Record, sum_counts, FACTOR_5};
 
-fn test_record_counts_only_unfolded(line: &str, expect_unfolded: usize) {
-    let record = Record::new(line).expect("valid line");
-    // let count = record.count_possibilities();
-    // assert_eq!(count, expected, "record (prior to unfold)");
-
-    let record_unfolded = record.unfold(FACTOR_5);
-    let count_unfolded = record_unfolded.count_possibilities();
-    assert_eq!(count_unfolded, expect_unfolded, "unfolded record");
-}
-
 fn test_record_counts(line: &str, (expected, expect_unfolded): (usize, usize)) {
     let record = Record::new(line).expect("valid line");
     let count = record.count_possibilities();
@@ -63,10 +53,7 @@ fn sample_input_record_4() {
 
 #[test]
 fn sample_input_record_5() {
-    super::test_record_count("????.######..#####. 1,6,5", 4);
-    // TODO
-    // let expected = todo!();
-    // test_record_counts("????.######..#####. 1,6,5", (4, expected));
+    test_record_counts("????.######..#####. 1,6,5", (4, 2500));
 }
 
 #[test]
@@ -85,26 +72,20 @@ fn sample_input_record_6_pretest() {
     // ---
     // 10 ##....#
     super::test_record_count("??????? 2,1", 10);
-    // TODO
-    // todo!();
-    // // unfolded length is 7*5 + 4 = 39
-    // // 1. ##.# (len 4, possibilities 0..=35 -> 36)
-    // // 2. ##..# (len 5, possibilities 0..=34 -> 35)
-    // // so 36 + 35 + 34 + .. + 1 = 36 * 35 / 2 = 630
-    // test_record_counts("??????? 2,1", (10, 630));
+}
+#[test]
+fn sample_input_record_6_pretest_unfold() {
+    todo!("needs caching");
+    // unfolded length is 7*5 + 4 = 39
+    // 1. ##.# (len 4, possibilities 0..=35 -> 36)
+    // 2. ##..# (len 5, possibilities 0..=34 -> 35)
+    // so 36 + 35 + 34 + .. + 1 = 36 * 35 / 2 = 630
+    test_record_counts("??????? 2,1", (10, 630));
 }
 
 #[test]
 fn sample_input_record_6() {
-    // vvvv--- `true` for BACON usage
-    if true {
-        super::test_record_count("?###???????? 3,2,1", 10);
-        // TODO
-        // test_record_counts("?###???????? 3,2,1", (10, 506250));
-    } else {
-        // TODO resume HERE
-        test_record_counts_only_unfolded("?###???????? 3,2,1", 506250);
-    }
+    test_record_counts("?###???????? 3,2,1", (10, 506250));
 }
 
 #[test]
@@ -121,21 +102,20 @@ fn sample_input() {
     assert_eq!(sum, 21);
 }
 
-//     TODO
-//     #[test]
-//     fn sample_input_unfolded() {
-//         let input = "???.### 1,1,3
-// .??..??...?##. 1,1,3
-// ?#?#?#?#?#?#?#? 1,3,1,6
-// ????.#...#... 4,1,1
-// ????.######..#####. 1,6,5
-// ?###???????? 3,2,1
-// ";
-//         let records = Record::parse_lines(input).unwrap();
-//         let unfolded = records
-//             .into_iter()
-//             .map(|record| record.unfold(FACTOR_5))
-//             .collect::<Vec<_>>();
-//         let sum = sum_counts(&unfolded);
-//         assert_eq!(sum, 525152);
-//     }
+#[test]
+fn sample_input_unfolded() {
+    let input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+";
+    let records = Record::parse_lines(input).unwrap();
+    let unfolded = records
+        .into_iter()
+        .map(|record| record.unfold(FACTOR_5))
+        .collect::<Vec<_>>();
+    let sum = sum_counts(&unfolded);
+    assert_eq!(sum, 525152);
+}
